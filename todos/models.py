311235976fc278a70c6d280b71ecfc54a034ee08
Nicode1010 <nicolasrios@unicauca.edu.co>
2024-09-23 16:48:58 -0500

    Avance 80%

--- todos/models.py
+++ todos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-
 class Todo(models.Model):
-    tittle = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     done = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
-    
+    def __str__(self):
+        return self.title
--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from todos import views
 
-
 urlpatterns = [
-    path('todos/', views.TodosAPIView.as_view()),
-    path('todo/<int:pk>/', views.TodoAPIView.as_view()),
+    path('todos/', views.TodosAPIView.as_view(), name='todos-list-create'),
+    path('todo/<int:pk>/', views.TodoAPIView.as_view(), name='todo-detail'),
 ]
